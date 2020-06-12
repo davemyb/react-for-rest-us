9482ef6323f46afc3bede6021f1cb41122b750fb
Dave Myburgh <david.m@myplanet.com>
2020-06-11 20:06:40 -0400

    Lesson 51: Deleting a post.

--- app/components/ViewSinglePost.js
+++ app/components/ViewSinglePost.js
@@ -1,14 +1,18 @@
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom'
+import React, { useEffect, useState, useContext } from 'react'
+import { Link, useParams, withRouter } from 'react-router-dom'
 import Axios from 'axios'
 import ReactMarkdown from 'react-markdown'
 import ReactTooltip from 'react-tooltip'
+import StateContext from '../StateContext'
+import DispatchContext from '../DispatchContext'
 
 import Page from './Page'
 import LoadingDotsIcon from './LoadingDotsIcon'
 import NotFound from './NotFound'
 
-function ViewSinglePost () {
+function ViewSinglePost (props) {
+  const appState = useContext(StateContext)
+  const appDispatch = useContext(DispatchContext)
   const { id } = useParams()
   const [isLoading, setIsLoading] = useState(true)
   const [post, setPost] = useState()
@@ -43,21 +47,47 @@ function ViewSinglePost () {
   const date = new Date(post.createdDate)
   const dateFormatted = `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`
 
+  function isOwner () {
+    if (appState.loggedIn) {
+      return appState.user.username === post.author.username
+    }
+    return false
+  }
+
+  async function deleteHandler () {
+    const areYouSure = window.confirm('Do you really want to delete this post?')
+    if (areYouSure) {
+      try {
+        const response = await Axios.delete(`/post/${id}`, { data: { token: appState.user.token } })
+        if (response.data === 'Success') {
+          // Display message that it was deleted.
+          appDispatch({ type: 'flashMessage', value: 'Post was successfully deleted.' })
+          // Redirect to profile page.
+          props.history.push(`/profile/${appState.user.username}`)
+        }
+      } catch (e) {
+        console.log(e.response.data)
+      }
+    }
+  }
+
   return (
     <Page title={post.title}>
       <div className='d-flex justify-content-between'>
         <h2>{post.title}</h2>
-        <span className='pt-2'>
-          <Link to={`/post/${post._id}/edit`} data-tip='Edit' data-for='edit' className='text-primary mr-2'>
-            <i className='fas fa-edit' />
-          </Link>
-          <ReactTooltip id='edit' className='custom-dash-tooltip' />
-          {' '}
-          <Link to={`/post/${post._id}/delete`} data-tip='Delete' data-for='delete' className='delete-post-button text-danger'>
-            <i className='fas fa-trash' />
-          </Link>
-          <ReactTooltip id='delete' className='custom-dash-tooltip' />
-        </span>
+        {isOwner() && (
+          <span className='pt-2'>
+            <Link to={`/post/${post._id}/edit`} data-tip='Edit' data-for='edit' className='text-primary mr-2'>
+              <i className='fas fa-edit' />
+            </Link>
+            <ReactTooltip id='edit' className='custom-dash-tooltip' />
+            {' '}
+            <Link onClick={deleteHandler} data-tip='Delete' data-for='delete' className='delete-post-button text-danger'>
+              <i className='fas fa-trash' />
+            </Link>
+            <ReactTooltip id='delete' className='custom-dash-tooltip' />
+          </span>
+        )}
       </div>
 
       <p className='text-muted small mb-4'>
@@ -74,4 +104,4 @@ function ViewSinglePost () {
   )
 }
 
-export default ViewSinglePost
+export default withRouter(ViewSinglePost)
