ed002b71c7b187ca1dc3ee854680cf2383280f35
Dave Myburgh <david.m@myplanet.com>
2020-06-14 22:46:30 -0400

    Lessong 53: React transitions for the search overlay.

--- app/Main.js
+++ app/Main.js
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react'
 import ReactDOM from 'react-dom'
 import { useImmerReducer } from 'use-immer'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { CSSTransition } from 'react-transition-group'
 import Axios from 'axios'
 import StateContext from './StateContext'
 import DispatchContext from './DispatchContext'
@@ -104,7 +105,9 @@ function Main () {
               <NotFound />
             </Route>
           </Switch>
-          {state.isSearchOpen ? <Search /> : ''}
+          <CSSTransition timeout={330} in={state.isSearchOpen} classNames='search-overlay' unmountOnExit>
+            <Search />
+          </CSSTransition>
           <Footer />
         </BrowserRouter>
       </DispatchContext.Provider>
